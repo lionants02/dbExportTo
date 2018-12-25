feeabafbbc1588a107d7a607060faac64ccb745d
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-12-25 16:50:41 +0700

    add getHint

--- src/main/kotlin/ffc/airsync/exportdb/Controller.kt
+++ src/main/kotlin/ffc/airsync/exportdb/Controller.kt
@@ -11,10 +11,12 @@ class Controller {
         val diseaseList = jhcisDbDao.getDisease()
         val homeHealthType = jhcisDbDao.getHomeHealthType()
         val specialPP = jhcisDbDao.getSpecialPpType()
+        val template = jhcisDbDao.getTemplate()
 
         writeFile(diseaseList.toJson(), "Disease.json")
         writeFile(homeHealthType.toJson(), "HomeHealthType.json")
         writeFile(specialPP.toJson(), "specialPP.json")
+        writeFile(template.toJson(), "template.json")
     }
 
     private fun writeFile(strData: String, fileName: String) {
--- src/main/kotlin/ffc/airsync/exportdb/QueryData.kt
+++ src/main/kotlin/ffc/airsync/exportdb/QueryData.kt
@@ -1,5 +1,6 @@
 package ffc.airsync.exportdb
 
+import ffc.entity.Template
 import ffc.entity.healthcare.CommunityService.ServiceType
 import ffc.entity.healthcare.Disease
 import ffc.entity.healthcare.SpecialPP.PPType
@@ -8,4 +9,5 @@ interface QueryData {
     fun getDisease(): List<Disease>
     fun getHomeHealthType(): List<ServiceType>
     fun getSpecialPpType(): List<PPType>
+    fun getTemplate(): List<Template>
 }
